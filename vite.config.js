5f051d8b77140d60520a0107c0bf9a308448907a
StanfordPines <pines10110@gmail.com>
2025-06-28 11:51:57 +0300

    Немного поменял структуру файлов

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import Components from "unplugin-react-components/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  }
-})
+    plugins: [
+        react(),
+        Components({
+            dirs: ["src/components"],
+            extensions: ["jsx"],
+            deep: true,
+            dts: true,
+        }),
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "src"),
+        },
+    },
+});
